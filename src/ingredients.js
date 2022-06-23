bbc2755ef952e50aabb70e1d114c36725a9ce696
Sungmin Ashley Ro <sungminro92@gmail.com>
2022-06-23 19:18:26 -0400

    added ingredients sheet

---
+++ src/ingredients.js
@@ -0,0 +1,242 @@
+export const ingredients = [
+    {
+        "id": 1,
+        "name": "Fish sauce",
+        "color": "a66646"
+    },
+    {
+        "id": 2,
+        "name": "Ground red pepper",
+        "color": "de6e33"
+    },
+    {
+        "id": 3,
+        "name": "Sugar",
+        "color": "f9f4e4"
+    },
+    {
+        "id": 4,
+        "name": "Minced green onion",
+        "color": "8db255"
+    },
+    {
+        "id": 5,
+        "name": "Gochujang",
+        "color": "770000"
+    },
+    {
+        "id": 6,
+        "name": "Vineager",
+        "color": "fcf1d2"
+    },
+    {
+        "id": 7,
+        "name": "Water",
+        "color": "d4f1f9"
+    },
+    {
+        "id": 8,
+        "name": "Ginger juice",
+        "color": "f3e6b1"
+    },
+    {
+        "id": 9,
+        "name": "Soy sauce",
+        "color": "3c1e1e"
+    },
+    {
+        "id": 10,
+        "name": "Minced garlic",
+        "color": "f2e9d2"
+    },
+    {
+        "id": 11,
+        "name": "Sesame oil",
+        "color": "d7bf99"
+    },
+    {
+        "id": 12,
+        "name": "Soy bean paste",
+        "color": "ab5336"
+    },
+    {
+        "id": 13,
+        "name": "Sesame",
+        "color": "ede2cc"
+    },
+    {
+        "id": 14,
+        "name": "Honey",
+        "color": "ffdb8c"
+    },
+    {
+        "id": 15,
+        "name": "Salt",
+        "color": "cdd7ce"
+    },
+    {
+        "id": 16,
+        "name": "Cooking wine",
+        "color": "772f37"
+    },
+    {
+        "id": 17,
+        "name": "Corn syrup",
+        "color": "fddcab"
+    },
+    {
+        "id": 18,
+        "name": "Milk",
+        "color": "fdfff5"
+    },
+    {
+        "id": 19,
+        "name": "Pepper oil",
+        "color": "8e3c36"
+    },
+    {
+        "id": 20,
+        "name": "Minced cilantro",
+        "color": "537150"
+    },
+    {
+        "id": 21,
+        "name": "Minced parsley",
+        "color": "4faa6c"
+    },
+    {
+        "id": 22,
+        "name": "Wine vinegar",
+        "color": "fcf1d2"
+    },
+    {
+        "id": 23,
+        "name": "Red pepper flakes",
+        "color": "3bb112a"
+    },
+    {
+        "id": 24,
+        "name": "Minced onion",
+        "color": "ece2d4"
+    },
+    {
+        "id": 25,
+        "name": "Lemon juice",
+        "color": "ffffec"
+    },
+    {
+        "id": 26,
+        "name": "Lime juice",
+        "color": "f0ebbb"
+    },
+    {
+        "id": 27,
+        "name": "Olive oil",
+        "color": "a0955d"
+    },
+    {
+        "id": 28,
+        "name": "White wine",
+        "color": "f9e8c0"
+    },
+    {
+        "id": 29,
+        "name": "Red wine",
+        "color": "823a3e"
+    },
+    {
+        "id": 30,
+        "name": "Minced shallot",
+        "color": "c6e0b3"
+    },
+    {
+        "id": 31,
+        "name": "Heavy cream",
+        "color": "e9ddc6"
+    },
+    {
+        "id": 32,
+        "name": "Minced herbs",
+        "color": "c6d782"
+    },
+    {
+        "id": 33,
+        "name": "Butter",
+        "color": "fdeaac"
+    },
+    {
+        "id": 34,
+        "name": "Pear juice",
+        "color": "f2e4bd"
+    },
+    {
+        "id": 35,
+        "name": "Mustard",
+        "color": "eab41e"
+    },
+    {
+        "id": 36,
+        "name": "Orange juice",
+        "color": "fca43c"
+    },
+    {
+        "id": 37,
+        "name": "Pineapple juice",
+        "color": "ffe07b"
+    },
+    {
+        "id": 38,
+        "name": "Chili sauce",
+        "color": "bf5b4e"
+    },
+    {
+        "id": 39,
+        "name": "Tomato sauce",
+        "color": "b21807"
+    },
+    {
+        "id": 40,
+        "name": "Ketchup",
+        "color": "a82d1f"
+    },
+    {
+        "id": 41,
+        "name": "Black pepper",
+        "color": "878888"
+    },
+    {
+        "id": 42,
+        "name": "Balsamic vineagar",
+        "color": "4b4644"
+    },
+    {
+        "id": 43,
+        "name": "Mayonnaise",
+        "color": "f9f5e5"
+    },
+    {
+        "id": 44,
+        "name": "Peanut butter",
+        "color": "c8a173"
+    },
+    {
+        "id": 45,
+        "name": "Soju",
+        "color": "ffffff"
+    },
+    {
+        "id": 46,
+        "name": "Oyster sauce",
+        "color": "2a1d0e"
+    },
+    {
+        "id": 47,
+        "name": "Teriyaki sauce",
+        "color": "3b2f2f"
+    },
+    {
+        "id": 48,
+        "name": "Gravy sauce",
+        "color": "86644f"
+    }
+]
